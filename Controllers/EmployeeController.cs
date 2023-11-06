984d04a73bd38522165ddd77945e9a37e8d82eaf
Guruprasad S K <115786145+Guruprasad-SK@users.noreply.github.com>
2023-11-06 17:36:30 +0530

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace LayoutMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        [Route("Employees")]
+        public ActionResult GetList()
+        {
+            return View("GetList","_NewLayout");
+        }
+        [Route("NewEmployee")]
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+    }
+}
